b3f876627a73537f42ca20a28b83c8037f8c5a94
mohammadrifat-hossain <mohammadrifat.hossain.rk@gmail.com>
2025-02-25 23:06:58 +0600

    updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "export",
 };
 
 export default nextConfig;
